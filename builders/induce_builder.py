bc611648d79a350895eb7c18266ae74c657e3c61
Ahn <sungsahn@88e9fe777c7f.ant.amazon.com>
2018-08-05 21:52:47 +0100

    change config

--- builders/induce_builder.py
+++ builders/induce_builder.py
@@ -83,7 +83,6 @@ class InducerBuilder:
         log_liks_pseudo_grads = tf.gradients(log_liks_pseudo, [weights])[0]
         fisher = (log_liks_grads-log_liks_pseudo_grads)/self.config.train_data_size
         fisher = tf.reduce_mean(tf.square(fisher))
-        fisher_grad = tf.gradients(fisher, [x_pseudo])[0]
         fisher_opt_op = tf.train.GradientDescentOptimizer(
             learning_rate=self.config.fisher_learning_rate).minimize(fisher)
 
@@ -105,31 +104,31 @@ class InducerBuilder:
 
         tf.add_to_collection('error', error)
 
-def mala(thetas, log_probs_fn, lr, nb_walk_per_step):
+def mala(weights, log_probs_fn, lr, nb_walk_per_step):
     avg_acceptance_rate_ = 0.0
-    thetas_ = thetas
-    log_probs_ = log_probs_fn(thetas_)
-    grads_ = tf.gradients(log_probs_, thetas_)[0]
+    weights_ = weights
+    log_probs_ = log_probs_fn(weights_)
+    grads_ = tf.gradients(log_probs_, weights_)[0]
     for step in range(nb_walk_per_step):
-        eps = tf.random_normal(thetas_.get_shape())
-        proposed_thetas_ = tf.stop_gradient(thetas_+lr*grads_+tf.sqrt(2*lr)*eps)
-        proposed_log_probs_ = log_probs_fn(proposed_thetas_)
-        proposed_grads_ = tf.gradients(proposed_log_probs_, proposed_thetas_)[0]
+        eps = tf.random_normal(weights_.get_shape())
+        proposed_weights_ = tf.stop_gradient(weights_+lr*grads_+tf.sqrt(2*lr)*eps)
+        proposed_log_probs_ = log_probs_fn(proposed_weights_)
+        proposed_grads_ = tf.gradients(proposed_log_probs_, proposed_weights_)[0]
 
         # add rejection step
         log_numer = proposed_log_probs_-0.25/lr*tf.reduce_sum(
-            tf.square(thetas_-proposed_thetas_-lr*proposed_grads_), axis=1)
+            tf.square(weights_-proposed_weights_-lr*proposed_grads_), axis=1)
         log_denom = log_probs_-0.5*tf.reduce_sum(tf.square(eps), axis=1)
         acceptance_rate = tf.clip_by_value(tf.exp(log_numer-log_denom), 0.0, 1.0)
 
         # accept samples and update related quantities
         u = tf.random_uniform(acceptance_rate.get_shape())
         accept = tf.less_equal(u, acceptance_rate)
-        thetas_ = tf.where(accept, proposed_thetas_, thetas_)
+        weights_ = tf.where(accept, proposed_weights_, weights_)
         log_probs_ = tf.where(accept, proposed_log_probs_, log_probs_)
 
         avg_acceptance_rate_ += tf.reduce_mean(acceptance_rate)/nb_walk_per_step
         if step < nb_walk_per_step-1:
             grads_ = tf.where(accept, proposed_grads_, grads_)
 
-    return tf.assign(thetas, thetas_), avg_acceptance_rate_
+    return tf.assign(weights, weights_), avg_acceptance_rate_
